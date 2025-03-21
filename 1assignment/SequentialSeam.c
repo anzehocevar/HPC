0bcd9f569a9d5c8b25d62741831418d2c7872516
Anže Hočevar <ah5393@hpc-login3.arnes.si>
2025-03-21 19:52:48 +0000

    finished sobel & grayscale output

--- 1assignment/SequentialSeam.c
+++ 1assignment/SequentialSeam.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <math.h>
 #include <omp.h>
+#include <limits.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #define STB_IMAGE_WRITE_IMPLEMENTATION
@@ -10,21 +13,16 @@
 // Use 0 to retain the original number of color channels
 #define COLOR_CHANNELS 0
 
-void copy_image(unsigned char *image_out, const unsigned char *image_in, size_t size)
-{
-
+void copy_image(unsigned char *image_out, const unsigned char *image_in, size_t size) {
     #pragma omp parallel for
-    for (size_t i = 0; i < size; ++i)
-    {
+    for (size_t i = 0; i < size; ++i) {
         image_out[i] = image_in[i];
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
-    if (argc < 3)
-    {
+    if (argc < 3) {
         printf("USAGE: sample input_image output_image\n");
         exit(EXIT_FAILURE);
     }
@@ -35,136 +33,108 @@ int main(int argc, char *argv[])
     snprintf(image_in_name, 255, "%s", argv[1]);
     snprintf(image_out_name, 255, "%s", argv[2]);
 
-    // Load image from file and allocate space for the output image
     int width, height, cpp;
     unsigned char *image_in = stbi_load(image_in_name, &width, &height, &cpp, COLOR_CHANNELS);
 
-    if (image_in == NULL)
-    {
-        printf("Error reading loading image %s!\n", image_in_name);
+    if (image_in == NULL) {
+        printf("Error loading image %s!\n", image_in_name);
         exit(EXIT_FAILURE);
     }
+
     printf("Loaded image %s of size %dx%d.\n", image_in_name, width, height);
-    const size_t datasize = width * height * cpp * sizeof(unsigned char);
-    unsigned char *image_out = (unsigned char *)malloc(datasize);
-    
-    //Print the number of threads
+    size_t datasize = (size_t)width * height * cpp;
+    unsigned char *image_out = (unsigned char *)malloc(datasize * sizeof(unsigned char));
+    unsigned char *energy = (unsigned char *)malloc(width * height * sizeof(unsigned char));
+
+    if (!image_out || !energy) {
+        printf("Memory allocation failed!\n");
+        stbi_image_free(image_in);
+        exit(EXIT_FAILURE);
+    }
+
     #pragma omp parallel
     {
         #pragma omp single
-        printf("Using %d threads",omp_get_num_threads());
+        printf("Using %d threads\n", omp_get_num_threads());
     }
 
-    
     // Copy the image
     copy_image(image_out, image_in, datasize);
 
-    for(int steps = 0;steps < 128;steps++){
-        // calculate energy
-        for(int i = 0;i < height;i++){
-            for(int j = 0;j < width;j++){
-                double Gx = -image_out[(i - 1) * width * cpp + (j - 1) * cpp]
-                        - 2 * image_out[i * width * cpp + (j - 1) * cpp]
-                        - image_out[(i + 1) * width * cpp + (j - 1) * cpp]
-                        + image_out[(i - 1) * width * cpp + (j + 1) * cpp]
-                        + 2 * image_out[i * width * cpp + (j + 1) * cpp]
-                        + image_out[(i + 1) * width * cpp + (j + 1) * cpp];
+    // Energy Calculation
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            double Gx[3] = {0.0, 0.0, 0.0};
+            double Gy[3] = {0.0, 0.0, 0.0};
 
-                double Gy = image_out[(i - 1) * width * cpp + (j - 1) * cpp] 
-                        + 2 * image_out[(i - 1) * width * cpp + j * cpp] 
-                        + image_out[(i - 1) * width * cpp + (j + 1) * cpp]
-                        - image_out[(i + 1) * width * cpp + (j - 1) * cpp] 
-                        - 2 * image_out[(i + 1) * width * cpp + j * cpp] 
-                        - image_out[(i + 1) * width * cpp + (j + 1) * cpp];
+            for (int c = 0; c < cpp; c++) {
+                // Handle out of bounds conditions
+                int i_minus_1 = (i - 1 < 0) ? 0 : i - 1;
+                int i_plus_1 = (i + 1 >= height) ? height - 1 : i + 1;
+                int j_minus_1 = (j - 1 < 0) ? 0 : j - 1;
+                int j_plus_1 = (j + 1 >= width) ? width - 1 : j + 1;
     
-                double energy = sqrt(Gx * Gx + Gy * Gy);
-                image_out[i * width * cpp + j * cpp] = (unsigned char)energy;
+                // Sobel operator 
+                Gx[c] = -image_out[i_minus_1 * width * cpp + j_minus_1 * cpp + c]
+                        - 2 * image_out[i * width * cpp + j_minus_1 * cpp + c]
+                        - image_out[i_plus_1 * width * cpp + j_minus_1 * cpp + c]
+                        + image_out[i_minus_1 * width * cpp + j_plus_1 * cpp + c]
+                        + 2 * image_out[i * width * cpp + j_plus_1 * cpp + c]
+                        + image_out[i_plus_1 * width * cpp + j_plus_1 * cpp + c];
+    
+                Gy[c] = image_out[i_minus_1 * width * cpp + j_minus_1 * cpp + c]
+                        + 2 * image_out[i_minus_1 * width * cpp + j * cpp + c]
+                        + image_out[i_minus_1 * width * cpp + j_plus_1 * cpp + c]
+                        - image_out[i_plus_1 * width * cpp + j_minus_1 * cpp + c]
+                        - 2 * image_out[i_plus_1 * width * cpp + j * cpp + c]
+                        - image_out[i_plus_1 * width * cpp + j_plus_1 * cpp + c];
             }
+
+            double energy_val = (sqrt(Gx[0] * Gx[0] + Gy[0] * Gy[0]) +
+                                 sqrt(Gx[1] * Gx[1] + Gy[1] * Gy[1]) +
+                                 sqrt(Gx[2] * Gx[2] + Gy[2] * Gy[2])) / 3.0;
+            
+            energy[i * width + j] = (unsigned char)energy_val;
         }
-
-        // remember path
-        int *path = (int *)malloc(sizeof(int) * height);
-
-        // calculate path cost
-        int cost = INT_MAX;
-        int col = 0;
-        for (int i = 0; i < width; i++)
-        {
-            if (image_out[(height - 1) * width * cpp + i * cpp] < cost)
-            {
-                cost = image_out[(height - 1) * width * cpp + i * cpp];
-                col = i;
-            }
-        }
-
-
-        path[0] = col;
-        for (int i = 1; i < height; i++)
-        {
-            int min = INT_MAX;
-            int min_col = 0;
-            // look at neighbours
-            for (int j = -1; j < 2; j++)
-            {
-                if (col + j >= 0 && col + j < width)
-                {
-                    if (image_out[(height - i) * width * cpp + (col + j) * cpp] < min)
-                    {
-                        min = image_out[(height - i) * width * cpp + (col + j) * cpp];
-                        min_col = col + j;
-                    }
-                }
-            }
-            col = min_col;
-            path[i] = col;
-        }
-
-        // remove the cheapest path
-        // but what happens when the talk isnt cheap
-        // and it is what it is?
-        // what i mean by that?
-        // hillary clinton is a lizard
-        //
-
-        // remove the cheapest path
-        for (int i = 0; i < height; i++)
-        {
-            for (int j = path[i]; j < width - 1; j++)
-            {
-                for (int c = 0; c < cpp; c++)
-                {
-                    image_out[i * width * cpp + j * cpp + c] = image_out[i * width * cpp + (j + 1) * cpp + c];
-                }
-            }
-        }
-        width--;
-
-        // remove the path
-        free(path);
-        
-
     }
-    
+
+    // Grayscale enery image
+    // Normalize energy values to 0-255 range
+    unsigned char *energy_image = (unsigned char *)malloc(width * height * sizeof(unsigned char));
+    double max_energy = 0.0;
+
+    // Find the maximum energy value for normalization
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (energy[i * width + j] > max_energy) {
+                max_energy = energy[i * width + j];
+            }
+        }
+    }
+
+    // Scale energy values to fit in 0-255
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            energy_image[i * width + j] = (unsigned char)((energy[i * width + j] / max_energy) * 255);
+        }
+    }
+
+    // Save the energy image as a grayscale PNG
+    stbi_write_png("energy.png", width, height, 1, energy_image, width);
+
+    // Free allocated memory for energy image
+    free(energy_image);
+
+    printf("Energy image saved as energy.png\n");
+
+    // Update final image size and save the result
+    stbi_write_png(image_out_name, width, height, cpp, image_out, width * cpp);
 
 
-
-
-    if (!strcmp(file_type, "png"))
-        stbi_write_png(image_out_name, width, height, cpp, image_out, width * cpp);
-    else if (!strcmp(file_type, "jpg"))
-        stbi_write_jpg(image_out_name, width, height, cpp, image_out, 100);
-    else if (!strcmp(file_type, "bmp"))
-        stbi_write_bmp(image_out_name, width, height, cpp, image_out);
-    else
-        printf("Error: Unknown image format %s! Only png, bmp, or bmp supported.\n", file_type);
-
-
-    // Free the image
     stbi_image_free(image_in);
-    free(image_in);
     free(image_out);
+    free(energy);
 
+    printf("Finished seam carving.\n");
     return 0;
 }
-
-

d76914aeeb60c619ac2a0c3fd15515f556a604dd
Anže Hočevar <ah5393@hpc-login3.arnes.si>
2025-05-08 22:03:49 +0200

    MPI sequential version

--- 3assignment/gray_scott.cu
+++ 3assignment/gray_scott.cu
@@ -56,6 +56,48 @@ double gray_scott2D(gs_config config){
     Write a 2D Gray-Scott simulation in C/C++ using CUDA, OpenMPI, and OpenMP.
     */
 
+    for(int it = 0;it < iterations; it++){
+        // Update U and V using the Gray-Scott model
+        #pragma omp parallel for collapse(2)
+        for(int i = 0;i < grid_size; i++){
+            for(int j = 0;j < grid_size;j++){
+                // Get the indices of the neighbors
+                int up = (i - 1 + grid_size) % grid_size;
+                int down = (i + 1) % grid_size;
+                int left = (j - 1 + grid_size) % grid_size;
+                int right = (j + 1) % grid_size;
+
+                // Compute the Laplacian
+                float laplacian_U = U[IDX(up, j, grid_size)] + U[IDX(down, j, grid_size)] +
+                                    U[IDX(i, left, grid_size)] + U[IDX(i, right, grid_size)] -
+                                    4 * U[IDX(i, j, grid_size)];
+
+                float laplacian_V = V[IDX(up, j, grid_size)] + V[IDX(down, j, grid_size)] +
+                                    V[IDX(i, left, grid_size)] + V[IDX(i, right, grid_size)] -
+                                    4 * V[IDX(i, j, grid_size)];
+
+                // Update U and V
+                U_next[IDX(i, j, grid_size)] = U[IDX(i, j, grid_size)] +
+                                                dt * (du * laplacian_U - U[IDX(i, j, grid_size)] * V[IDX(i, j, grid_size)] * V[IDX(i, j, grid_size)] +
+                                                f * (1 - U[IDX(i, j, grid_size)]));
+
+                V_next[IDX(i, j, grid_size)] = V[IDX(i, j, grid_size)] +
+                                                dt * (dv * laplacian_V + U[IDX(i,j,size)] * V[IDX(i,j,size)] * V[IDX(i,j,size)] -
+                                                (f + k) * V[IDX(i,j,size)]);
+            }
+        }
+
+        // Swap pointers
+        float *temp = U;
+        U = U_next;
+        U_next = temp;
+
+        temp = V;
+        V = V_next;
+        V_next = temp;
+    }
+
+
     // return average concentartion of V
     double avgV = 0.0;
     for (int i = 0; i < size * size; i++) {
